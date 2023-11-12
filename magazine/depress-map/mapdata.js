08c839589ba5c2e89fe031cb788e4998bb36c35a
Nguyễn Hữu Đại <dainh@gemvietnam.com>
2023-11-13 00:01:39 +0700

    abc

---
+++ magazine/depress-map/mapdata.js
@@ -0,0 +1,460 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "600", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    VNM429: {
+      name: "Quảng Ninh",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM444: {
+      name: "Tây Ninh",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Có 56.8% học sinh bị trầm cảm; 23,2% học sinh trầm cảm nhẹ; 24,8% học sinh trầm cảm vừa và 8,8% học sinh có dấu hiệu trầm cảm nặng."
+    },
+    VNM450: {
+      name: "Điện Biên",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM451: {
+      name: "Đông Bắc",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM452: {
+      name: "Thái Nguyên",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Đối với những bệnh nhân mắc các bệnh trầm cảm tỷ lệ hiện nay cao chiếm từ 15 đến 20% tỷ lệ dân số, tức là trong 100 người có khoảng 15 đến 20 người bị bệnh trầm cảm (1/4/2019, dân số tỉnh Thái Nguyên là 1.286.751 người)"
+    },
+    VNM453: {
+      name: "Lai Châu",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỷ lệ hiện mắc trầm cảm và một số yếu tố liên quan với trầm cảm ở bệnh nhân điều trị nghiện chất dạng thuốc phiện (CDTP) bằng Methadone và Buprenorphine ở 3 tỉnh miền núi phía Bắc Việt Nam là Lai Châu, Điện Biên và Sơn La.Tỷ lệ trầm cảm (điểm PHQ 2 ≥ 2) chung là 7,3%, nhóm người bệnh Methadone 1,7%, nhóm người bệnh Buprenorphine 10,2% (p < 0,05).\n(Người bệnh điều trị methadone và buprenorphine)"
+    },
+    VNM454: {
+      name: "Lạng Sơn",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "74,8% bệnh nhân ung thư có biểu hiện mắc bệnh trầm cảm, bao gồm 16,8% trầm cảm nhẹ, 49% trầm cảm vừa và trầm cảm nặng 9%. Trầm cảm cao nhất trong số những bệnh nhân bị ung thư dạ dày"
+    },
+    VNM455: {
+      name: "Sơn La",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM456: {
+      name: "Thanh Hóa",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Theo báo cáo của Bệnh viện Tâm thần Thanh Hóa, hiện tại mỗi ngày có khoảng 200 người đến đây khám và tư vấn sức khỏe tâm thần. Trước đây, độ tuổi của trầm cảm thường là 60-65 tuổi, thì nay có nhiều bệnh nhân từ 18-35 tuổi, thậm chí có cả tuổi vị thành niên.<br/>\nTỷ lệ rối loạn stress, lo âu, trầm cảm lần lượt là 41,7%, 49,0%, 43,6%, chủ yếu ở mức độ nhẹ và trung bình. Có khoảng 1/3 số trẻ đã từng nghĩ đến tự làm đau bản thân và 10% trẻ đã tự làm đau bản thân. 25% số trẻ đã từng nghĩ đến tự tử và 1,4% số trẻ đã thực hiện tự tử nhưng không thành công."
+    },
+    VNM457: {
+      name: "Tuyên Quang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM458: {
+      name: "Yên Bái",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Trong năm 2021, trung bình 1 ngày khoa Khám bệnh của Bệnh viện Tâm thần tỉnh Yên Bái tiếp nhận hơn 100 bệnh nhân đến khám và điều trị. Trong đó có tới 50 - 60% có triệu chứng rối loạn về tâm lý như stress, trầm cảm, lo âu và rối loạn cảm xúc.\n"
+    },
+    VNM459: {
+      name: "Hòa Bình",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Theo số liệu thống kê của Trung tâm Kiểm soát bệnh tật tỉnh, hiện tổng số bệnh nhân đang quản lý và điều trị trong toàn tỉnh là 3.148 bệnh nhân. Trong đó bệnh nhân tâm thần phân liệt là 1.952, động kinh là 1.096 và trầm cảm là 93 bệnh nhân."
+    },
+    VNM460: {
+      name: "Hải Dương",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỷ lệ mắc rối loạn trầm cảm chiếm 14,7%. Tỷ lệ rối loạn trầm cảm nặng là 1%, tỷ lệ rối loạn trầm cảm trung bình là 9,1%. Tỷ lệ rối loạn trầm cảm cao trong độ tuổi từ 60-69."
+    },
+    VNM4600: {
+      name: "Hải Phòng",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Tỷ lệ mắc rối loạn trầm cảm ở đối tượng nghiên cứu là 28,7%. Trong đó, tỷ lệ mắc rối loạn trầm cảm của học sinh khối 12 là 35,8%; khối 11 là 22,6% và khối 10 là 27,9%. Tỷ lệ học sinh mắc rối loạn trầm cảm mức độ nhẹ là 22,3%, mức độ vừa là 4,4% và mức độ nặng là 2,0%."
+    },
+    VNM461: {
+      name: "Hưng Yên",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Theo một nghiên cứu do Bệnh viện Nhi T.Ư thực hiện mới đây với 726 học sinh tại Hưng Yên, tỷ lệ trầm cảm với các mức độ khác nhau ở học sinh tại Hưng Yên là 18,6%. Tỷ lệ các em có lo âu là 36,5% tại Hưng Yên."
+    },
+    VNM462: {
+      name: "Hà Nội",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Tỷ lệ học sinh có vấn đề trầm cảm là 27% và chủ yếu là mức độ vừa và nhẹ. Học sinh nữ mắc nhiều hơn nam và khối lớp cuối cấp có nguy cơ bị trầm cảm cao hơn so với đầu cấp. Yếu tố môi trường gia đình, các trải nghiệm cá nhân tiêu cực có ảnh hưởng đến nguy cơ mắc trầm cảm."
+    },
+    VNM463: {
+      name: "Bắc Ninh",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM464: {
+      name: "Vĩnh Phúc",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Khảo sát của Bệnh viện Tâm thần Vĩnh Phúc tại 9 trường Trung học phổ thông trên địa bàn TP Vĩnh Yên, huyện Tam Dương, Bình Xuyên trong các năm 2018, 2019, 2020 với tổng số 5.308 học sinh, kết quả có 5.8% học sinh được khảo sát có nguy cơ của rối loạn trầm cảm."
+    },
+    VNM466: {
+      name: "Ninh Bình",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "2.000 người dân được tiếp cận và cung cấp kiến thức về bệnh trầm cảm, qua khám sàng lọc phát hiện hơn 400 người có dấu hiệu trầm cảm ở các mức độ.  (khoảng 20%)"
+    },
+    VNM467: {
+      name: "Hà Nam",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỷ lệ trầm cảm ở người bệnh STM (suy thận mạn) chạy TNT chu kỳ cao hơn nhiều so với tỷ lệ trầm cảm trong quần thể chung. Trầm cảm và STM có mối liên quan chặt chẽ với nhau."
+    },
+    VNM468: {
+      name: "Nam Định",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM469: {
+      name: "Phú Thọ",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM470: {
+      name: "Bắc Giang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỉnh Bắc Giang có hơn 3,5 nghìn bệnh nhân tâm thần, động kinh đang được quản lý, điều trị (tăng 100 người bệnh so với cùng kỳ năm 2019).<br/>\nTrong đó có từ 20-30% người mắc tâm thần nặng như: Tâm thần phân liệt; rối loạn hành vi; động kinh; trầm cảm; thiểu năng trí tuệ. Bệnh lý có xu hướng gia tăng ở người trẻ. Tỷ lệ phụ nữ mắc bệnh cao hơn nam giới."
+    },
+    VNM471: {
+      name: "Thái Bình",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Đối tượng trầm cảm chiếm tỉ lệ 4,9%; trong đó tỉ lệ trầm cảm ở nữ giới 7,3% cao hơn gần gấp 3 lần ở nam giới 2,5%."
+    },
+    VNM474: {
+      name: "Hà Tĩnh",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Kết quả nghiên cứu cho thấy nguy cơ mắc trầm cảm theo thang đo CES-D ở học sinh là 57,6%. Học sinh nữ có nguy cơ trầm cảm gấp 1,84 lần (KTC 95%: 1,19 – 2,93) so với học sinh nam"
+    },
+    VNM475: {
+      name: "Nghệ An",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Kết quả cho thấy tỷ lệ học sinh có dấu hiệu trầm cảm là 38,2%, lo âu là 39,2%."
+    },
+    VNM476: {
+      name: "Quảng Bình",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "7,6% người cao tuổi có các rối loạn trầm cảm (Điểm GDS ≥6 trong tổng số 15 điểm tối đa) và 3,8% được chẩn đoán mắc trầm cảm"
+    },
+    VNM477: {
+      name: "Đắk Lắk",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Trung bình một tháng bệnh viện Tâm thần tỉnh Đắk Lắk tiếp nhận khám và điều trị cho khoảng 20 đến 30 trường hợp bệnh trầm cảm ở tuổi vị thành niên"
+    },
+    VNM478: {
+      name: "Gia Lai",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM479: {
+      name: "Khánh Hòa",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Số liệu được thu thập từ chọn mẫu toàn bộ học sinh khối lớp 12 niên học 2017-2018. Kết quả có 55,4% đối tượng nghiên cứu có biểu hiện trầm cảm, mức độ nhẹ là 17,4%; vừa 22,3% và nặng là 7,7%."
+    },
+    VNM480: {
+      name: "Lâm Đồng",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Theo Trung tâm Kiểm soát bệnh tật Lâm Đồng, hiện đang quản lý 27 bệnh nhân trầm cảm"
+    },
+    VNM481: {
+      name: "Ninh Thuận",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM482: {
+      name: "Phú Yên",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM483: {
+      name: "Bình Dương",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM4834: {
+      name: "Tiền Giang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Một khảo sát do Bệnh viện Chợ Rẫy thực hiện tại Bệnh viện Hồi sức Covid-19 TP. Thủ Đức từ tháng 9 cũng cho thấy, 53,3% bệnh nhân tại đây bị rối loạn lo âu, 16,7% stress và 20% trầm cảm. Đặc biệt, những bệnh nhân từng thở oxy lưu lượng cao qua mũi (HFNC), thở oxy qua mặt nạ, hoặc thở máy, thì tỷ lệ trầm cảm và rối loạn lo âu lên tới 66,7%"
+    },
+    VNM4835: {
+      name: "Đắk Nông",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM484: {
+      name: "Bình Phước",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Kết quả phân tích trên 217 đối tượng thỏa tiêu chí đưa vào, tỷ lệ rối loạn trầm cảm ở người cao tuổi là 20,7%."
+    },
+    VNM485: {
+      name: "Bình Định",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỉ lệ trầm cảm sau sinh còn khá cao (12,1%), tình trạng trầm cảm sau sinh có liên quan đến mối quan hệ vợ chồng, gia đình bên chồng, tiền sử thai kì và sự hỗ trợ của người thân trong gia đình."
+    },
+    VNM486: {
+      name: "Kon Tum",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM487: {
+      name: "Quảng Nam",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Ghi nhận trên 148 HS đến từ những trường THPT khác nhau tại thị xã Điện Bàn (tỉnh Quảng Nam) sau kỳ thi tốt nghiệp THPT 2021, theo Nguyễn Thị Lan Vi (chuyên ngành Tâm lý lâm sàng, Trường ĐH Đông Á, Đà Nẵng), tỷ lệ mắc trầm cảm nhẹ là 51% và ở trầm cảm nặng, con số này lên đến 25%. Nữ sinh cũng chỉ ra nguyên nhân dẫn đến trầm cảm phần lớn đến từ quá trình học tập (36,9%) và các mối quan hệ xã hội (31,5%)."
+    },
+    VNM488: {
+      name: "Quảng Ngãi",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tại Bệnh viện tâm thần Quảng Ngãi, mỗi ngày có 150-200 bệnh nhân đến khám, chữa bệnh. Trong đó, có đến 50 ca là những bệnh nhân mắc bệnh trầm cảm cần được tư vấn, giúp đỡ"
+    },
+    VNM489: {
+      name: "Quảng Trị",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM490: {
+      name: "Thừa Thiên - Huế",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM491: {
+      name: "Đà Nẵng",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Có 27,5% có nguy cơ trầm cảm, 19,3% biểu hiện nguy cơ trầm cảm ở mức độ nhẹ, 3,9% ở mức độ vừa, 4,3% ở mức độ nặng"
+    },
+    VNM495: {
+      name: "Bà Rịa - Vũng Tàu",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Bác sĩ Bảo Bái, Phó Giám đốc Bệnh viện Tâm thần tỉnh cho biết, bệnh trầm cảm đang có xu hướng ngày càng gia tăng và chiếm khoảng 20% trong số các rối loạn về sức khỏe tâm thần. Hằng năm, Bệnh viện Tâm thần tỉnh tiếp nhận điều trị hơn 20 ca bị trầm cảm."
+    },
+    VNM496: {
+      name: "Bình Thuận",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Độ tuổi trung bình của các đối tượng tham gia là 49,9 ± 4,7 tuổi. Tỷ lệ các đối tượng có dấu hiệu trầm cảm là 16,9%.\nKhảo sát với phụ nữ trong độ tuổi mãn kinh"
+    },
+    VNM497: {
+      name: "Đông Nam Bộ",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM498: {
+      name: "An Giang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM499: {
+      name: "Cần Thơ",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "16% đối tượng tham gia nghiên cứu mắc trầm cảm\nNgười 18-60 tuổi"
+    },
+    VNM500: {
+      name: "Ðồng Tháp",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: " Tỷ lệ mắc trầm cảm ở người già, nữ cao hơn nam và càng lớn tuổi tỷ lệ khác biệt này nhỏ hơn.  Một nghiên cứu cho thấy tỷ lệ này tăng ở những người vào nhà dưỡng lão mắc trầm cảm trong năm đầu tỷ lệ là 54 %. Trầm cảm đơn cực: 10- 38%. Nhẹ: 35.3%, vừa 51.9%, nặng 12.7%. Thường bị bỏ sót (40-60%)"
+    },
+    VNM501: {
+      name: "Hồ Chí Minh",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Kết quả điều tra ở một số trường Trung học phổ thông tại thành phố Hồ Chí Minh, có đến 21% các em bị trầm cảm và 7,5 % các ca tự tử thành công là trẻ em từ 14 đến 17 tuổi."
+    },
+    VNM502: {
+      name: "Kiên Giang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Tỷ lệ trầm cảm của nhân viên y tế là 13,9%"
+    },
+    VNM503: {
+      name: "Long An",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM504: {
+      name: "Bến Tre",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "27,7% học sinh mắc trầm cảm"
+    },
+    VNM505: {
+      name: "Hậu Giang",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Trong 718 học sinh tham gia nghiên cứu, kết quả cho thấy tỷ lệ các rối loạn stress, lo âu, trầm cảm lần lượt là: 52,1%; 63,8%; 42,1%. Tỷ lệ mắc một tình trạng là 24%, hai tình trạng là 26% và mắc cả ba tình trạng là 27,3%. "
+    },
+    VNM506: {
+      name: "Bạc Liêu",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM507: {
+      name: "Cà Mau",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM508: {
+      name: "Sóc Trăng",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Có 56.8% học sinh bị trầm cảm; 23,2% học sinh trầm cảm nhẹ; 24,8% học sinh trầm cảm vừa và 8,8% học sinh có dấu hiệu trầm cảm nặng."
+    },
+    VNM509: {
+      name: "Trà Vinh",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Có 91 bệnh nhân tham gia nghiên cứu, trong đó nữ chiếm 64,8%. Đa số có trình độ học vấn tiểu học (33%), thuộc nhóm tuổi 60-70 (68,1%), đã kết hôn (63,7%) và sống chung cả gia đình (89%). Tỷ lệ bệnh nhân có trầm cảm là 20,9% (19 người)"
+    },
+    VNM510: {
+      name: "Vĩnh Long",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Không có dữ liệu"
+    },
+    VNM511: {
+      name: "Cao Bằng",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Toàn tỉnh hiện có 1.862 bệnh nhân tâm thần, trong đó, 17 bệnh nhân trầm cảm"
+    },
+    VNM512: {
+      name: "Hà Giang",
+      color: "#D9D9D9",
+      hover_color: "#EDD160",
+      description: "Theo báo cáo của Khoa phòng chống bệnh không lây nhiễm, Trung tâm Kiểm soát bệnh tật tổng số bệnh nhân tâm thần được quản lý tại cơ sở là 2.153 bệnh nhân trong đó bệnh nhân trầm cảm là 18 bệnh nhân"
+    },
+    VNM5483: {
+      name: "Lào Cai",
+      color: "#FF914D",
+      hover_color: "#EB5F1A",
+      description: "Tỉ lệ học sinh có biểu hiện trầm cảm  là 25,8%. Học sinh có biểu hiện trầm cảm chủ yếu là mức độ nhẹ và vừa (42,4% và 40%), mức độ nặng và rất nặng chiếm 14,1% và 3,5%."
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
