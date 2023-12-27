996f116e3bceaac00d5c114deba443cf84cd35b8
Nguyễn Hữu Đại <dainh@gemvietnam.com>
2023-12-28 02:56:21 +0700

    a

---
+++ magazine/copy-hospital-map-plus/mapdata.js
@@ -0,0 +1,460 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "600", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    VNM429: {
+      name: "Quảng Ninh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Bảo vệ sức khỏe tâm thần Quảng Ninh <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; X"
+    },
+    VNM444: {
+      name: "Tây Ninh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Tây Ninh <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM450: {
+      name: "Điện Biên",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Điện Biên <br>\n&#x2022; Khoa Tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM451: {
+      name: "Bắc Kạn",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện đa khoa tỉnh Bắc Kạn <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM452: {
+      name: "Thái Nguyên",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Thái Nguyên <br>\n&#x2022; Khoa Tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM453: {
+      name: "Lai Châu",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện tỉnh Lai Châu <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM454: {
+      name: "Lạng Sơn",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Lạng Sơn <br>\n&#x2022; Khoa Tâm thần – Thần kinh <br>\n&#x2022; X"
+    },
+    VNM455: {
+      name: "Sơn La",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Sơn La <br>\n&#x2022; Khoa Công tác xã hội <br>\n&#x2022; X"
+    },
+    VNM456: {
+      name: "Thanh Hóa",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Thanh Hóa <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM457: {
+      name: "Tuyên Quang",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Tuyên Quang <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM458: {
+      name: "Yên Bái",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Yên Bái <br>\n&#x2022; Khoa Điều trị rối loạn tâm thần <br>\n&#x2022; X"
+    },
+    VNM459: {
+      name: "Hòa Bình",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Hòa Bình <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM460: {
+      name: "Hải Dương",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Hải Dương <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM4600: {
+      name: "Hải Phòng",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Hải Phòng <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; X"
+    },
+    VNM461: {
+      name: "Hưng Yên",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện tâm thần Hưng Yên <br>\n&#x2022; Khoa tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM462: {
+      name: "Hà Nội",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Viện Sức khỏe Tâm thần Quốc gia - Bệnh viện Bạch Mai <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Quân y 103 <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Tâm thần Hà Nội <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Đa khoa Hồng Ngọc <br>\n&#x2022; Khoa Tâm thần và Sức khỏe tâm thần <br>\n&#x2022; O\n&#x2022; <br>\n&#x2022; Bệnh viện Đa khoa quốc tế Tâm Anh <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; O\n&#x2022; <br>\n\"&#x2022; Bệnh viện Đa khoa quốc tế Vinmec <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; O"
+    },
+    VNM463: {
+      name: "Bắc Ninh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Sức khỏe Tâm thần Bắc Ninh <br>\n&#x2022; Khoa Nhi <br>\n&#x2022; X"
+    },
+    VNM464: {
+      name: "Vĩnh Phúc",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Vĩnh Phúc <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM466: {
+      name: "Ninh Bình",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Ninh Bình <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM467: {
+      name: "Hà Nam",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Hà Nam <br>\n&#x2022; không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM468: {
+      name: "Nam Định",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Nam Định <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM469: {
+      name: "Phú Thọ",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Phú Thọ <br>\n&#x2022; Khoa tâm lý lâm sàng <br>\n&#x2022; X"
+    },
+    VNM470: {
+      name: "Bắc Giang",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Bắc Giang <br>\n&#x2022; Khoa Tâm bệnh trẻ em <br>\n&#x2022; X <br>\n&#x2022; Phòng khám đa khoa Hợp Nhất - Bắc Giang <br>\n&#x2022; Chuyên khoa tư vấn Tâm lý <br>\n&#x2022; O"
+    },
+    VNM471: {
+      name: "Thái Bình",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Thái Bình <br>\n&#x2022; Khoa tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM474: {
+      name: "Hà Tĩnh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Hà Tĩnh <br>\n&#x2022; Khoa Nhi <br>\n&#x2022; X"
+    },
+    VNM475: {
+      name: "Nghệ An",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Nghệ An <br>\n&#x2022; Khoa Tâm lý<br>\n&#x2022; X"
+    },
+    VNM476: {
+      name: "Quảng Bình",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Quảng Bình <br>\n&#x2022; Khoa nội <br>\n&#x2022; X"
+    },
+    VNM477: {
+      name: "Đắk Lắk",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần Đắk Lắk <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM478: {
+      name: "Gia Lai",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Gia Lai <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM479: {
+      name: "Khánh Hòa",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Chuyên khoa Tâm thần <br>\n&#x2022; Khoa Tâm thần trẻ em và vị thành niên <br>\n&#x2022; X"
+    },
+    VNM480: {
+      name: "Lâm Đồng",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Lâm Đồng <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM481: {
+      name: "Ninh Thuận",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Chuyên khoa Da liễu - Tâm thần tỉnh Ninh Thuận <br>\n&#x2022; Khoa Nội <br>\n&#x2022; X"
+    },
+    VNM482: {
+      name: "Phú Yên",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Trạm chuyên khoa Tâm thần Phú Yên <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM483: {
+      name: "Bình Dương",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện đa khoa Medic Bình Dương <br>\n&#x2022; Khoa Nhi <br>\n&#x2022; O"
+    },
+    VNM4834: {
+      name: "Tiền Giang",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần Tiền Giang <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM4835: {
+      name: "Đắk Nông",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Đắk Nông <br>\n&#x2022; Khoa Tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM484: {
+      name: "Bình Phước",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Bình Phước <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM485: {
+      name: "Bình Định",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Bình Định <br>\n&#x2022; Khoa Tâm bệnh <br>\n&#x2022; X"
+    },
+    VNM486: {
+      name: "Kon Tum",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Kon Tum <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM487: {
+      name: "Quảng Nam",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Quảng Nam <br>\n&#x2022; Khoa Khám bệnh <br>\n&#x2022; X"
+    },
+    VNM488: {
+      name: "Quảng Ngãi",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Quảng Ngãi <br>\n&#x2022; Khoa Tâm căn - Tâm thần trẻ em - Phục hồi chức năng <br>\n&#x2022; X"
+    },
+    VNM489: {
+      name: "Quảng Trị",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Trung tâm Kiểm soát bệnh tật tỉnh Quảng Trị - Khoa Phòng chống bệnh không lây nhiễm - phòng khám chuyên khoa tâm thần <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM490: {
+      name: "Thừa Thiên - Huế",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Thừa Thiên Huế <br>\n&#x2022; Khoa nhi <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Trung ương Huế <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM491: {
+      name: "Đà Nẵng",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Đà Nẵng <br>\n&#x2022; Khoa Tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM495: {
+      name: "Bà Rịa-Vũng Tàu",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Bà Rịa - Vũng Tàu <br>\n&#x2022; Đo không tuyển được bác sĩ tâm thần nhi nên bệnh viện chưa triển khai Khoa Điều trị trẻ em.\n<br>\n&#x2022; X"
+    },
+    VNM496: {
+      name: "Bình Thuận",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Bình Thuận <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM497: {
+      name: "Đồng Nai",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Trung Ương 2 <br>\n&#x2022; Khoa Tâm thần trẻ em <br>\n&#x2022; X"
+    },
+    VNM498: {
+      name: "An Giang",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh An Giang <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM499: {
+      name: "Cần Thơ",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Phòng khám Tâm Lý Cần Thơ <br>\n&#x2022; <br>\n&#x2022; Bệnh viện Tâm thần Cần Thơ <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; X"
+    },
+    VNM500: {
+      name: "Đồng Tháp",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Đồng Tháp <br>\n&#x2022; Khoa Nội Tâm thần <br>\n&#x2022; X"
+    },
+    VNM501: {
+      name: "TP Hồ Chí Minh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần TP. Hồ Chí Minh <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Đại học Y dược TPHCM <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X\n&#x2022; <br>\n&#x2022; Bệnh viện Tâm thần Trung ương 2 <br>\n&#x2022; Khoa Tâm thần trẻ em\n&#x2022; X"
+    },
+    VNM502: {
+      name: "Kiên Giang",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Kiên Giang <br>\n&#x2022; Khoa Tâm lý <br>\n&#x2022; X"
+    },
+    VNM503: {
+      name: "Long An",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Long An <br>\n&#x2022; Khoa Nhi <br>\n&#x2022; X"
+    },
+    VNM504: {
+      name: "Bến Tre",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Bến Tre <br>\n&#x2022; Khoa Tâm thần Nhi <br>\n&#x2022; X"
+    },
+    VNM505: {
+      name: "Hậu Giang",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Chuyên khoa Tâm thần và Da liễu tỉnh Hậu Giang <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM506: {
+      name: "Bạc Liêu",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Bạc Liêu <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM507: {
+      name: "Cà Mau",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa Cà Mau <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM508: {
+      name: "Sóc Trăng",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện đa khoa tỉnh Sóc Trăng <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM509: {
+      name: "Trà Vinh",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Trà Vinh <br>\n&#x2022; Khoa Tâm thần<br>\n&#x2022; X"
+    },
+    VNM510: {
+      name: "Vĩnh Long",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Tâm thần tỉnh Vĩnh Long <br>\n&#x2022; Không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM511: {
+      name: "Cao Bằng",
+      color: "#b6b6b6",
+      hover_color: "#555555",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Cao Bằng <br>\n&#x2022; Khoa tâm thần của bệnh viện đa khoa không có chuyên khoa khám chữa bệnh trầm cảm cho trẻ vị thành niên <br>\n&#x2022; X"
+    },
+    VNM512: {
+      name: "Hà Giang",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Tâm thần Hà Giang <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    },
+    VNM5483: {
+      name: "Lào Cai",
+      color: "#8AC4E6",
+      hover_color: "#0484CF",
+      description: "&#x2022; Bệnh viện Đa khoa tỉnh Lào Cai <br>\n&#x2022; Khoa Tâm thần <br>\n&#x2022; X"
+    }
+  },
+  locations: {},
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
